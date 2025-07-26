a67db9a6fde8ef11524373690b509a173563e1cb
omer mukhtar <omermukhtar55@gmail.com>
2025-07-26 23:47:52 +0300

    Remove Firebase dependencies and related code from pubspec.yaml, Auth.dart, and GeneratedPluginRegistrant.swift; update pubspec.lock accordingly.

--- lib/src/Service/Auth.dart
+++ lib/src/Service/Auth.dart
@@ -1,17 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-
-
+// import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthServise {
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Sign In anonymouse
   Future signInAnon() async {
     try {
-      UserCredential result = await _auth.signInAnonymously();
-      User? user = result.user;
+      // UserCredential result = await _auth.signInAnonymously();
+      // User? user = result.user;
+      print("Firebase auth temporarily disabled");
     } catch (e) {
       print(e.toString());
       return null;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
-import firebase_analytics
-import firebase_auth
-import firebase_core
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
